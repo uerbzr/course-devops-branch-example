b710bf7f23202e5d6e74e989875f7241fed0d143
uerbzr <nigel.sibbert@gmail.com>
2025-07-05 21:08:40 +0100

    updated

--- scripts/about.sh
+++ scripts/about.sh
@@ -1,5 +1,8 @@
+echo ""
+echo ""
 echo "ℹ️ℹ️ℹ️ About ℹ️ℹ️ℹ️"
 echo "Welcome to the pre-commit hook script!"
 echo "By Nigel"
 echo ""
+
 
--- scripts/commit.sh
+++ scripts/commit.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 echo ""
-echo "###################################################"
+echo ""
 echo "🔍🔍🔍 Running .NET tests before commit...🔍🔍🔍"
 
 dotnet test --verbosity minimal
 status=$?
-
+echo ""
 if [ $status -ne 0 ]; then
+  echo ""
   echo "❌❌❌ Commit blocked: Tests failed. ❌❌❌"
   exit 1
 else
+  echo ""
   echo "✅✅✅ All tests passed. Proceeding with commit. ✅✅✅"
   exit 0
 fi
+echo ""
+
