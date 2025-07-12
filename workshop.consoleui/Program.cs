c100a1872466411faeafffef4491601197d2f334
uerbzr <nigel.sibbert@gmail.com>
2025-07-12 16:39:08 +0100

    finished feature

---
+++ workshop.consoleui/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using workshop.calculator;
+
+Console.WriteLine("Welcome to adder..");
+
+var numbers = new List<int>();
+Console.WriteLine("Enter numbers to add.. blank to finish");
+
+while (true)
+{
+    Console.Write("Enter number: ");
+    //string input = Console.ReadLine()?.Trim();
+    string? input = Console.ReadLine();
+
+    if (input is null) break;
+    if(input==string.Empty) break;
+
+
+    if (int.TryParse(input, out int number))
+    {
+        numbers.Add(number);
+    }
+    else
+    {
+        Console.WriteLine("Invalid input. Please enter a valid integer.");
+    }
+}
+
+Calculator calculator = new Calculator();
+Console.WriteLine($"Total: {calculator.Add(numbers)}");
